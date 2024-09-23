dc2410938c31c0591e5324a3a0b6996820413b00
andrew6072 <leducanh017@gmail.com>
2024-09-23 17:24:38 +0700

    file upload using multer, add .env file for email sending

--- demo_nodemailer.js
+++ demo_nodemailer.js
@@ -1,24 +1,38 @@
-var nodemailer = require('nodemailer');
+const express = require('express');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-var transporter = nodemailer.createTransport({
+
+// Create an instance of express
+const app = express();
+
+// Configure the transporter for nodemailer
+const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'leducanh017@gmail.com',
-        pass: '*******'
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS  // Replace with your actual password or use environment variables for security
     }
 });
 
-var mailOptions = {
+// Email options
+const mailOptions = {
     from: 'leducanh017@gmail.com',
     to: 'leducanh017mephi@gmail.com',
     subject: 'Sending Email using Node.js',
-    text: 'That was easy!'
+    text: 'First email sent by Node.js server. That was easy!'
 };
 
+// Send email when server starts
 transporter.sendMail(mailOptions, function(error, info){
     if (error) {
-        console.log(error);
+        console.log('Error sending email:', error);
     } else {
-        console.log('Email sent: ' + info.response);
+        console.log('Email sent successfully:', info.response);
     }
+});
+
+// Start the server
+app.listen(8080, () => {
+    console.log('Server running on http://localhost:8080');
 });
